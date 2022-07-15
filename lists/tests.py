d3fe33910f42fb3831f7294187a528a5c19ae0fe
sacman08 <sacmail08@gmail.com>
2022-07-15 19:46:53 -0400

    Changes to tests.py

--- lists/tests.py
+++ lists/tests.py
@@ -2,18 +2,21 @@ from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
 from lists.views import home_page
+from django.template.loader import render_to_string
 
 class HomePageTest(TestCase):
     def test_root_url_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
-    def test_home_page_returns(self):
-        request = HttpRequest()
-        response = home_page(request)
+    def test_home_page_returns_correct_html(self):
+        response = self.client.get('/')
+        
         html = response.content.decode('utf8')
         self.assertTrue(html.startswith('<html>'))
         self.assertIn('<title>To-Do Lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        self.assertTrue(html.strip().endswith('</html>'))
+
+        self.assertTemplateUsed(response, 'home.html')
 
 
