f7eaf67d7f8d5bf0787831326b7578263a727ae8
sacman08 <sacmail08@gmail.com>
2022-07-15 21:19:45 -0400

    Front page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -23,7 +23,7 @@ class NewVisitorTest(unittest.TestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
                 inputbox.get_attribute('placeholder'),
-                'Enter a to-do item:'
+                'Enter a to-do item'
                 )
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
@@ -32,15 +32,12 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-                any(row.text == '1: Buy peacock feathers' for row in rows)
+                any(row.text == '1: Buy peacock feathers' for row in rows),
+                "New to-do item did not appear in table"
                 )
         self.fail('Fini!')
 
 
-    def test_list(self):
-        self.browser.get('http://localhost:8000')
-        self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish Him!')
 
 
 if __name__ == '__main__':
